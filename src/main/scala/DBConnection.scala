8e77b1e281f85de57be44c6891cd44b2b61f040c
Abhijay Patne <abhijaypatne@gmail.com>
2016-11-30 23:17:40 -0600

    added readfile()

--- src/main/scala/DBConnection.scala
+++ src/main/scala/DBConnection.scala
@@ -1,4 +1,5 @@
 import com.mongodb.casbah.{MongoClient, MongoClientURI}
+import scala.io.Source
 
 /**
   * Created by avp on 11/30/2016.
@@ -10,10 +11,22 @@ object DBConnection {
   val db = mongoClient("cs441project");
 
   def main(args: Array[String]): Unit = {
+    readFile("./documents/example.json");
+    insertRecord("movie", "");
     findAll("movie");
 
   }
 
+  def insertRecord(collectionName:String, jsonRecord:String): Unit ={
+    val collectionObject = db(collectionName);
+
+  }
+
+  def readFile(fileName:String): Unit ={
+    for (line <- io.Source.fromFile(fileName).getLines()) {
+      println(line)
+    }
+  }
   // given a collectionName, print all the entries of that collection
   def findAll(collectionName: String): Unit = {
     val collectionObject = db(collectionName);
