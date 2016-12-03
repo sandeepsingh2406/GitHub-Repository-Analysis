3735c5cc1036667ae59b443d4d678aacd5119fcf
Abhijay Patne <abhijaypatne@gmail.com>
2016-12-03 01:03:50 -0600

    added insertTopRepoLanguageTable() into mysql___()

--- src/main/scala/MySQLOperationAPIs.scala
+++ src/main/scala/MySQLOperationAPIs.scala
@@ -16,13 +16,34 @@ object MySQLOperationAPIs {
   //      ":" + ParameterConstants.mysqlDBPortNumber + "/" + ParameterConstants.mysqlDBName;
   //      "/" + ParameterConstants.mysqlDBName;
 
-  Class.forName(ParameterConstants.mysqlDriver);
-  println ("reached here");
-  var connection:Connection = DriverManager.getConnection(url, ParameterConstants.mysqlUserName, ParameterConstants.mysqlPassword);
-  println(insertTopRepoCommitsTable("sdf", 123, 123, 245));
-  def main(args: Array[String]): Unit ={
+  var connection:Connection = _;
+  try{
+    Class.forName(ParameterConstants.mysqlDriver);
+    connection = DriverManager.getConnection(url, ParameterConstants.mysqlUserName, ParameterConstants.mysqlPassword);
+  } catch {
+    case e => println("Exception in insertTopRepoCommitsTable()");
   }
 
+  def main(args: Array[String]): Unit ={
+    println(insertTopRepoCommitsTable("sdsdgf", 123, 123, 245));
+    println(insertTopRepoLanguageTable("Adfasdg", 324545456, "java", 1232454, 12));
+  }
+
+  def insertTopRepoLanguageTable(repoName:String, repoID:Long, language:String, numberOfLines:Long, numberOfFiles:Int): Int = {
+//    INSERT INTO `toprepolanguagetable`(`repoName`, `repoID`, `languge`, `numberOfLines`, `numberOfFiles`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5])
+    var result = -1;
+    val query = "INSERT INTO `toprepolanguagetable`(`repoName`, `repoID`, `language`, `numberOfLines`, `numberOfFiles`) VALUES (\"" +
+    repoName + "\", " + repoID + ", \"" + language.toLowerCase() + "\", " + numberOfLines + ", " + numberOfFiles + ");";
+    try {
+      val statement = connection.createStatement();
+      result = statement.executeUpdate(query);
+    } catch {
+      case e => {
+        println("Exception in insertTopRepoLanguageTable()" );
+      }
+    }
+    return result;
+  }
 
   // insert query for topRepoCommitsTable
   def insertTopRepoCommitsTable(repoName:String, repoID:Long, numberOfCommits:Int, numberOfFiles:Int): Int = {
